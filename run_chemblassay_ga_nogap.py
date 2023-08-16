5b171b1a5267049af0342a7e49b0f2704e669830
Greg Landrum <glandrum@ethz.ch>
2023-08-16 15:56:59 +0200

    more move to Ki

--- run_chemblassay_ga_nogap.py
+++ run_chemblassay_ga_nogap.py
@@ -9,19 +9,24 @@ import argparse
 
 train_frac_active = -1
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("assay")
-parser.add_argument("--strategy", "-s", choices=['DIVERSE', 'CLUSTERS_SPLIT'], default='CLUSTERS_SPLIT')
+parser.add_argument("target")
+parser.add_argument("--strategy",
+                    "-s",
+                    choices=['DIVERSE', 'CLUSTERS_SPLIT'],
+                    default='CLUSTERS_SPLIT')
 parser.add_argument("--popSize", "-p", type=int, default=500)
-parser.add_argument("--nGens", "-g", type=int, default=100)
-parser.add_argument("--useAlternateSpatialStatsObjectives","-a",default=False,action='store_true')
+parser.add_argument("--nGens", "-g", type=int, default=300)
+parser.add_argument("--useAlternateSpatialStatsObjectives",
+                    "-a",
+                    default=True,
+                    action='store_false')
 parser.add_argument("--numThreads", type=int, default=1)
-parser.add_argument("--balanced","-b",default=False,action="store_true")
+parser.add_argument("--balanced", "-b", default=False, action="store_true")
 args = parser.parse_args()
 
 # Name of the dataset to use:
-assay = args.assay
+target = args.target
 pop_size = args.popSize
 ngens = args.nGens
 strategy = args.strategy
@@ -36,12 +41,11 @@ if not altStats:
 # The rest of the setup code assumes that the data frame df has a column
 # named 'mol' with rdkit molecules. If you don't have that, you could just use a list
 # of molecules and adjust the code below to reflect that
-catalog = intake.open_catalog('./datasets/public_data.yaml')
-bioactivities = catalog.assays
+bioactivities = intake.open_catalog('./datasets/targets.yaml')
 
-ds = bioactivities[assay]
+ds = bioactivities[target]
 
-print('------------\nLoading data')
+print(f'------------  Target {target}\nLoading data')
 df = ds.read()
 
 # randomize the order of the molecules:
@@ -49,77 +53,46 @@ df = ds.read()
 df = df.sample(frac=1, random_state=0xf00d)
 
 # generate the binned activity values
-pActs = np.array(
-    [-1 * math.log10(x*1e-9) if x > 0 else 0 for x in df.standard_value])
-df['pActivity'] = pActs
+pActs = np.array(df.pchembl_value.to_list())
 if not isBalanced:
-    upper, lower = ga_lib.get_imbalanced_bins(pActs,active_inactive_offset=0.0)
+    upper, lower = ga_lib.get_imbalanced_bins(pActs,
+                                              active_inactive_offset=0.0)
 else:
-    upper, lower = ga_lib.get_imbalanced_bins(pActs,tgt_frac=0.4,active_inactive_offset=0.0)
+    upper, lower = ga_lib.get_imbalanced_bins(pActs,
+                                              tgt_frac=0.4,
+                                              active_inactive_offset=0.0)
 df = df[(pActs >= upper) | (pActs <= lower)]
-binned = [1 if x >= upper else 0 for x in df.pActivity]
+binned = [1 if x >= upper else 0 for x in pActs]
 df['active'] = ['active' if x == 1 else 'inactive' for x in binned]
 
 frac_active = np.sum(binned) / len(binned)
 
-for j,dfrac in enumerate(ga_lib.delta_test_active_frac_vals):
-    if not altStats:
-        for i,(target_F_val,target_G_val) in enumerate(ga_lib.target_FG_vals):
-            print(f'------------\nRunning GA, iter {target_F_val}, {target_G_val}, {dfrac:.2f}')
-            train_inds, tests_inds, res = ga_lib.run_GA_old(
-                df,
-                strategy,
-                pop_size,
-                ngens,
-                verbose=True,
-                numThreads=args.numThreads,
-                return_random_result=False,
-                smilesCol='canonical_smiles',
-                actCol='active',
-                targetTrainFracActive=-1,
-                targetTestFracActive=-1,
-                targetDeltaTestFracActive=dfrac,
-                targetFval=target_F_val,
-                targetGval=target_G_val,
-                skipDescriptors=False)
-
-            print(f"{len(train_inds)} solutions")
-
-            import pickle
-            import gzip
-            with gzip.open(
-                    f'./nogap_results_assay/{assay}_{strategy}.{pop_size}.{ngens}.scenario_{i}_{j}.pkl.gz',
-                    'wb+') as outf:
-                pickle.dump((train_inds, tests_inds), outf)
-                pickle.dump((res.F, res.G, res.X), outf)
+for j, dfrac in enumerate(ga_lib.delta_test_active_frac_vals):
+    if not isBalanced:
+        outfn = './nogap_alt_results_imbalanced'
     else:
-            print(f'------------\nRunning GA, iter {dfrac:.2f}')
-            train_inds, tests_inds, res = ga_lib.run_GA_SIMPD(
-                df,
-                strategy,
-                pop_size,
-                ngens,
-                verbose=True,
-                numThreads=args.numThreads,
-                return_random_result=False,
-                smilesCol='canonical_smiles',
-                actCol='active',
-                targetTrainFracActive=-1,
-                targetTestFracActive=-1,
-                targetDeltaTestFracActive=dfrac)
+        outfn = './nogap_alt_results_balanced'
 
-            print(f"{len(train_inds)} solutions")
+    outfn += f'/{target}_{strategy}.{pop_size}.{ngens}.altscenario_{j}.pkl.gz'
+    print(f'------------\nRunning GA, iter {dfrac:.2f}')
+    train_inds, tests_inds, res = ga_lib.run_GA_SIMPD(
+        df,
+        strategy,
+        pop_size,
+        ngens,
+        verbose=True,
+        numThreads=args.numThreads,
+        return_random_result=False,
+        smilesCol='canonical_smiles',
+        actCol='active',
+        targetTrainFracActive=-1,
+        targetTestFracActive=-1,
+        targetDeltaTestFracActive=dfrac)
 
-            import pickle
-            import gzip
-            if not isBalanced:
-                outfn = './nogap_alt_results_imbalanced_assay'
-            else:
-                outfn = './nogap_alt_results_balanced_assay'
+    print(f"{len(train_inds)} solutions")
 
-            outfn += f'/{assay}_{strategy}.{pop_size}.{ngens}.altscenario_{j}.pkl.gz'
-            with gzip.open(
-                    outfn,
-                    'wb+') as outf:
-                pickle.dump((train_inds, tests_inds), outf)
-                pickle.dump((res.F, res.G, res.X), outf)
+    import pickle
+    import gzip
+    with gzip.open(outfn, 'wb+') as outf:
+        pickle.dump((train_inds, tests_inds), outf)
+        pickle.dump((res.F, res.G, res.X), outf)
--- run_chemblassay_ml_nogap.py
+++ run_chemblassay_ml_nogap.py
@@ -7,12 +7,21 @@ import ga_lib_3 as ga_lib
 import sys
 import math
 
+from rdkit.Chem.MolStandardize import rdMolStandardize
+from rdkit import rdBase
+
+rdBase.DisableLog('rdApp.info')
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn import metrics
-from sklearn.metrics import balanced_accuracy_score,cohen_kappa_score
+from sklearn.metrics import balanced_accuracy_score, cohen_kappa_score
 
-def run_it_oob_optimization(oob_probs, labels_train, thresholds, ThOpt_metrics = 'Kappa'):
+
+def run_it_oob_optimization(oob_probs,
+                            labels_train,
+                            thresholds,
+                            ThOpt_metrics='Kappa'):
     """Optimize the decision threshold based on the prediction probabilities of the out-of-bag set of random forest.
     The threshold that maximizes the Cohen's kappa coefficient or a ROC-based criterion 
     on the out-of-bag set is chosen as optimal.
@@ -38,69 +47,77 @@ def run_it_oob_optimization(oob_probs, labels_train, thresholds, ThOpt_metrics =
         tscores = []
         # evaluate the score on the oob using different thresholds
         for thresh in thresholds:
-            scores = [1 if x>=thresh else 0 for x in oob_probs]
-            kappa = metrics.cohen_kappa_score(labels_train,scores)
-            tscores.append((np.round(kappa,3),thresh))
+            scores = [1 if x >= thresh else 0 for x in oob_probs]
+            kappa = metrics.cohen_kappa_score(labels_train, scores)
+            tscores.append((np.round(kappa, 3), thresh))
         # select the threshold providing the highest kappa score as optimal
         tscores.sort(reverse=True)
         thresh = tscores[0][-1]
     # Optmize the decision threshold based on the ROC-curve
     elif ThOpt_metrics == 'ROC':
         # ROC optimization with thresholds determined by the roc_curve function of sklearn
-        fpr, tpr, thresholds_roc = metrics.roc_curve(labels_train, oob_probs, pos_label=1)
-        specificity = 1-fpr
-        roc_dist_01corner = (2*tpr*specificity)/(tpr+specificity)
+        fpr, tpr, thresholds_roc = metrics.roc_curve(labels_train,
+                                                     oob_probs,
+                                                     pos_label=1)
+        specificity = 1 - fpr
+        roc_dist_01corner = (2 * tpr * specificity) / (tpr + specificity)
         thresh = thresholds_roc[np.argmax(roc_dist_01corner)]
     return thresh
 
 
-def test_rf_model(X,y,nIters=10,numThreads=1):
+def test_rf_model(X, y, nIters=10, numThreads=1):
     X = np.array(X)
     y = np.array(y)
 
-    sss = StratifiedShuffleSplit(n_splits = nIters, test_size=0.2, random_state=0xf00d)
+    sss = StratifiedShuffleSplit(n_splits=nIters,
+                                 test_size=0.2,
+                                 random_state=0xf00d)
     scores = []
     kappas = []
     balanced = []
     rocs = []
-    for train,test in sss.split(X,y):
-        X_train,X_test = X[train],X[test]
-        y_train,y_test = y[train],y[test]
-        cls = RandomForestClassifier(n_estimators=500,max_depth=15,min_samples_leaf=2,min_samples_split=4,
-                                     random_state=0xf00d,n_jobs=num_threads,oob_score=True)
-        cls.fit(X_train,y_train)
+    for train, test in sss.split(X, y):
+        X_train, X_test = X[train], X[test]
+        y_train, y_test = y[train], y[test]
+        cls = RandomForestClassifier(n_estimators=500,
+                                     max_depth=15,
+                                     min_samples_leaf=2,
+                                     min_samples_split=4,
+                                     random_state=0xf00d,
+                                     n_jobs=num_threads,
+                                     oob_score=True)
+        cls.fit(X_train, y_train)
 
         oobProbs = [x[1] for x in cls.oob_decision_function_]
-        thresh = run_it_oob_optimization(oobProbs,y_train,np.arange(0.10,0.55,0.05))
+        thresh = run_it_oob_optimization(oobProbs, y_train,
+                                         np.arange(0.10, 0.55, 0.05))
         probs = cls.predict_proba(X_test)
-        preds = [1 if x[1]>=thresh else 0 for x in probs]
-        kappas.append(cohen_kappa_score(y_test,preds))
-        balanced.append(balanced_accuracy_score(y_test,preds))
-        scores.append(cls.score(X_test,y_test))
+        preds = [1 if x[1] >= thresh else 0 for x in probs]
+        kappas.append(cohen_kappa_score(y_test, preds))
+        balanced.append(balanced_accuracy_score(y_test, preds))
+        scores.append(cls.score(X_test, y_test))
 
         auc = metrics.roc_auc_score(y_test, [x[1] for x in probs])
         rocs.append(auc)
 
-
-    print(rocs)    
-    return rocs,scores,kappas,balanced
-
+    print(rocs)
+    return rocs, scores, kappas, balanced
 
 
 import argparse
 
 train_frac_active = -1
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("assay")
+parser.add_argument("target")
 parser.add_argument("--numThreads", type=int, default=1)
-parser.add_argument("--balanced","-b",default=False,action="store_true")
-parser.add_argument("--baseline", default=False, action="store_true") # whether or not to use MFP0
+parser.add_argument("--balanced", "-b", default=False, action="store_true")
+parser.add_argument("--baseline", default=False,
+                    action="store_true")  # whether or not to use MFP0
 args = parser.parse_args()
 
 # Name of the dataset to use:
-assay = args.assay
+target = args.target
 num_threads = args.numThreads
 isBalanced = args.balanced
 doBaseline = args.baseline
@@ -110,10 +127,9 @@ doBaseline = args.baseline
 # The rest of the setup code assumes that the data frame df has a column
 # named 'mol' with rdkit molecules. If you don't have that, you could just use a list
 # of molecules and adjust the code below to reflect that
-catalog = intake.open_catalog('./datasets/public_data.yaml')
-bioactivities = catalog.assays
+bioactivities = intake.open_catalog('./datasets/targets.yaml')
 
-ds = bioactivities[assay]
+ds = bioactivities[target]
 
 print('------------\nLoading data')
 df = ds.read()
@@ -123,20 +139,21 @@ df = ds.read()
 df = df.sample(frac=1, random_state=0xf00d)
 
 # generate the binned activity values
-pActs = np.array(
-    [-1 * math.log10(x*1e-9) if x > 0 else 0 for x in df.standard_value])
-df['pActivity'] = pActs
+pActs = np.array(df.pchembl_value.to_list())
 if not isBalanced:
-    upper, lower = ga_lib.get_imbalanced_bins(pActs,active_inactive_offset=0.0)
+    upper, lower = ga_lib.get_imbalanced_bins(pActs,
+                                              active_inactive_offset=0.0)
 else:
-    upper, lower = ga_lib.get_imbalanced_bins(pActs,tgt_frac=0.4,active_inactive_offset=0.0)
+    upper, lower = ga_lib.get_imbalanced_bins(pActs,
+                                              tgt_frac=0.4,
+                                              active_inactive_offset=0.0)
 
 df = df[(pActs >= upper) | (pActs <= lower)]
-binned = [1 if x >= upper else 0 for x in df.pActivity]
+binned = [1 if x >= upper else 0 for x in df.pchembl_value]
 df['active'] = ['active' if x == 1 else 'inactive' for x in binned]
 
 mols = [
-    Chem.MolFromSmiles(tmp_smi)
+    rdMolStandardize.ChargeParent(Chem.MolFromSmiles(tmp_smi))
     for tmp_smi in df['canonical_smiles'].to_numpy(dtype=str)
 ]
 
@@ -147,9 +164,10 @@ else:
     generator = rdFingerprintGenerator.GetMorganGenerator(radius=0)
     fps = [generator.GetCountFingerprintAsNumPy(x) for x in mols]
 
-
 print('------------\nRunning RF experiments')
-rocs,scores,kappas,balanced = test_rf_model(fps,binned,numThreads=num_threads)
+rocs, scores, kappas, balanced = test_rf_model(fps,
+                                               binned,
+                                               numThreads=num_threads)
 
 import pickle
 import gzip
@@ -158,11 +176,9 @@ outfn = './'
 if doBaseline:
     outfn += 'mfp0_'
 if not isBalanced:
-    outfn += 'nogap_ml_results_imbalanced_assay'
+    outfn += 'nogap_ml_results_imbalanced'
 else:
-    outfn += 'nogap_ml_results_balanced_assay'
+    outfn += 'nogap_ml_results_balanced'
 
-with gzip.open(
-        f'{outfn}/{assay}_randomsplit.pkl.gz',
-        'wb+') as outf:
-    pickle.dump((rocs,scores,kappas,balanced), outf)
+with gzip.open(f'{outfn}/{target}_randomsplit.pkl.gz', 'wb+') as outf:
+    pickle.dump((rocs, scores, kappas, balanced), outf)
